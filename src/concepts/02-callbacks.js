2840f55cdf5584ec70662348895a66d12e36ca4c
David Martinez <sergiodavidbatresmartinez@gmail.com>
2025-01-29 11:16:47 -0600

    Agregar función para encontrar héroes y mostrar su nombre en el componente de callbacks

--- src/concepts/02-callbacks.js
+++ src/concepts/02-callbacks.js
@@ -1,10 +1,27 @@
+import { heroes } from './data/heroes.js';
+
 /**
  * 
  * @param {HTMLMediaElement} element 
  */
 export const  CollbacksComponent = (element) => {
-    console.log('CollbacksComponent');
+
+    const id = '5d86371fd55e2e2a30fe1ccb1';
+    findHero(id, (hero) => {
+        // el hero? es para desirle que si el hero no se encuentra que muestrel el undefined y no el error en consola
+        // el || sirve como un else si no se encuentra el heroe que mueste le mensaje 'no hay heroe'
+        element.innerHTML = hero?.name || 'no hay herroe'; 
+    } );
 
 };
+/**
+ * 
+ * @param {string} id
+ * @param {(herro: object) => void} callback
+ */
+const findHero = (id, callback) => {
+    
+    const hero = heroes.find (hero => hero.id === id);
 
-
+    callback(hero); 
+};
