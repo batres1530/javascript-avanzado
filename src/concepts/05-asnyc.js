c63502a3f0dc0357365001f93e3ce7c62f221c24
David Martinez <sergiodavidbatresmartinez@gmail.com>
2025-02-04 07:35:28 -0600

    Agregar función para buscar héroes y mostrar su nombre en el componente asíncrono

--- src/concepts/05-asnyc.js
+++ src/concepts/05-asnyc.js
@@ -1,4 +1,17 @@
-export const  asyncComponet = (element) => {
-    console.log('asyncComponet');
+import {heroes} from './data/heroes.js';
 
+
+export const  asyncComponet = (element) => {
+    const id1 = '5d86371fd55e2e2a30fe1ccb22';
+
+
+    findHero(id1)
+        .then( name  => element.innerHTML = name) 
+        .catch(error => element.innerHTML = error);
+};
+
+const findHero = async (id) => {
+
+    const hero = heroes.find(hero => hero.id === id);
+        return hero.name;
 };
