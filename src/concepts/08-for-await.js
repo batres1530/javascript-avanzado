5924cf6a6e66a6fe5d3eed59bab4cde599e48a8c
David Martinez <sergiodavidbatresmartinez@gmail.com>
2025-02-05 17:19:12 -0600

    Agregar lógica comentada para verificar la existencia de un héroe en el componente forAwait

--- src/concepts/08-for-await.js
+++ src/concepts/08-for-await.js
@@ -3,7 +3,16 @@ import { heroes } from './data/heroes';
 export const forAwaitComponent = async( element ) => {
 console.log('Inicio');
 
+    const id = '5d86371f1efebc31def272e2';
 
+    // if  ( await getHeroAsync(id) ) {
+
+    // element.innerHTML = `el heroe con id ${id} existe`;
+    // return;
+    // }
+   
+    //     element.innerHTML = `el heroe con id ${id} no existe`;
+        
 }
 
 const getHeroesAsync = ( heroIds ) => {
